3c2e83107bc84fcde520fc7f66458b74b62a7b2a
marcel-photo <marcel.bluemler@googlemail.com>
2017-11-21 22:21:56 +0100

    parameter tuner (fifa2trans & fut2trans) implemented

--- src/main/java/solution_player/IR_using_linear_combination_fut_transfermarkt.java
+++ src/main/java/solution_player/IR_using_linear_combination_fut_transfermarkt.java
@@ -1,8 +1,15 @@
 package solution_player;
 
 import java.io.File;
+import java.io.IOException;
 
-import blocker.PlayerBlockingFunctionName;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
+
+import blocker.PlayerBlockingFunctionBirthdate;
+import comparators.PlayerBirthdateComparatorJaccard;
 import comparators.PlayerHeightComparator;
 import comparators.PlayerNameComparatorJaccard;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Player;
@@ -22,68 +29,82 @@ import de.uni_mannheim.informatik.dws.winter.processing.Processable;
 
 public class IR_using_linear_combination_fut_transfermarkt {
 	public static void main(String[] args) throws Exception {
+		double score = 0.0;
+		double bestN = 0.0;
+		double bestH = 0.0;
+		double bestB = 0.0;
+		for (double i = 0.05; i < 1.5; i += 0.1) {
+			for (double j = 0.05; j < 1.5; j += 0.1) {
+				for (double k = 0.05; k < 1.5; k += 0.1) {
+					System.out.println("Name: " + i);
+					System.out.println("Height: " + j);
+					double tempscore = parameterTuner(i, j, k);
+					if (tempscore >= score) {
+						score = tempscore;
+						bestN = i;
+						bestH = j;
+						bestB = k;
+						System.out.println("-----------------------------" + score + "-----------------------------");
+					}
+				}
+			}
+		}
+		System.out.println("-----PARAMETER------");
+		System.out.println("Name: " + bestN);
+		System.out.println("Height: " + bestH);
+		System.out.println("Birthdate: " + bestB);
+		System.out.println("F1: " + score);
+	}
+
+	public static double parameterTuner(double name, double height, double bd) throws Exception {
 		// loading data
-		
+
 		HashedDataSet<Player, Attribute> dataTrans = new HashedDataSet<>();
 		new PlayerXMLReader().loadFromXML(new File("data/input/transfermarkt.xml"),
 				"/stadiums/stadium/clubs/club/players/player", dataTrans);
-		
-		
+
 		HashedDataSet<Player, Attribute> dataFut17 = new HashedDataSet<>();
 		new PlayerXMLReader().loadFromXML(new File("data/input/fut17.xml"),
 				"/stadiums/stadium/clubs/club/players/player", dataFut17);
-
-	
 
 		// create a matching rule
 		LinearCombinationMatchingRule<Player, Attribute> matchingRule = new LinearCombinationMatchingRule<>(0.5);
 
 		// add comparators
-		matchingRule.addComparator(new PlayerNameComparatorJaccard(), 0.9);
-		matchingRule.addComparator(new PlayerHeightComparator(), 0.2);
-
-		// create a blocker (blocking strategy)
-		NoBlocker<Player, Attribute> blocker = new NoBlocker<Player, Attribute>();
+		matchingRule.addComparator(new PlayerNameComparatorJaccard(), name);
+		matchingRule.addComparator(new PlayerHeightComparator(), height);
+		matchingRule.addComparator(new PlayerBirthdateComparatorJaccard(), bd);
 
 		StandardRecordBlocker<Player, Attribute> blocker2 = new StandardRecordBlocker<Player, Attribute>(
-				new PlayerBlockingFunctionName());
-		
+				new PlayerBlockingFunctionBirthdate());
 
 		// Initialize Matching Engine
 		MatchingEngine<Player, Attribute> engine = new MatchingEngine<>();
 
 		// Execute the matching
-		Processable<Correspondence<Player, Attribute>> correspondences = engine.runIdentityResolution(
-				dataFut17, dataTrans, null, matchingRule,
-				blocker2);
+		Processable<Correspondence<Player, Attribute>> correspondences = engine.runIdentityResolution(dataFut17,
+				dataTrans, null, matchingRule, blocker2);
 
 		// write the correspondences to the output file
 		new CSVCorrespondenceFormatter().writeCSV(new File("data/output/fut17_2_trans_correspondences.csv"),
 				correspondences);
-		
+
 		// load the gold standard (test set)
-				MatchingGoldStandard gsTest = new MatchingGoldStandard();
-				gsTest.loadFromCSVFile(new File(
-						"data/goldstandard/gs_fut17_2_trans.csv"));
+		MatchingGoldStandard gsTest = new MatchingGoldStandard();
+		gsTest.loadFromCSVFile(new File("data/goldstandard/gs_fut17_2_trans.csv"));
 
-				// evaluate your result
-				MatchingEvaluator<Player, Attribute> evaluator = new MatchingEvaluator<Player, Attribute>(true);
-				Performance perfTest = evaluator.evaluateMatching(correspondences.get(),
-						gsTest);
+		// evaluate your result
+		MatchingEvaluator<Player, Attribute> evaluator = new MatchingEvaluator<Player, Attribute>(true);
+		Performance perfTest = evaluator.evaluateMatching(correspondences.get(), gsTest);
 
-				// check which errors were made
-				new ErrorAnalysis().printFalsePositives(correspondences, gsTest);
-				new ErrorAnalysis().printFalseNegatives(dataFut17, dataTrans, correspondences, gsTest);
-				
-				// print the evaluation result
-				System.out.println("Fut17 <-> Transfermarkt");
-				System.out
-						.println(String.format(
-								"Precision: %.4f\nRecall: %.4f\nF1: %.4f",
-								perfTest.getPrecision(), perfTest.getRecall(),
-								perfTest.getF1()));
-		
+		// check which errors were made
+		new ErrorAnalysis().printFalsePositives(correspondences, gsTest);
+		new ErrorAnalysis().printFalseNegatives(dataFut17, dataTrans, correspondences, gsTest);
 
-		
+		// print the evaluation result
+		System.out.println("Fut17 <-> Transfermarkt");
+		System.out.println(String.format("Precision: %.4f\nRecall: %.4f\nF1: %.4f", perfTest.getPrecision(),
+				perfTest.getRecall(), perfTest.getF1()));
+		return perfTest.getF1();
 	}
 }
